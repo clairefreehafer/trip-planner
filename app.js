4eb1d71b25d19b4e0a41782f84457ff8a07c1607
Claire Freehafer <claire.freehafer@gmail.com>
2017-01-27 12:48:55 -0500

    initialization

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const db = require('./models');
+const router = require('/routes');
+
+const app = express();
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', { noCache: true });
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use('/', router);
+
+// catch 404 (i.e., no route was hit) and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// handle all errors (anything passed into `next()`)
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	console.error(err);
+	res.render('error', err);
+});
+
+// db.sync({}).then(function () {
+	app.listen(3000, function() {
+		console.log('Server is listening on port 3000');
+	});
+// });
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
+module.exports = router;
+
+router.get('/', function(req, res, next) {
+	res.render('index');
+});
+
+
