4a6292ce72c0a02ae9a07d61e1c296454a5452fa
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-01-27 16:45:16 -0500

    updated frontend dependencies

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, './public')));
 
+app.use('/bootstrap', express.static(path.join(__dirname, './node_modules/bootstrap/dist')));
+
+app.use('/jquery', express.static(path.join(__dirname, './node_modules/jquery/dist')));
+
 app.use('/', router);
 
 // catch 404 (i.e., no route was hit) and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
+const db = require('../models');
+
+
+const Sequelize = require('sequelize');
+const Place = db.models.place; 
+const Activity = db.models.activity; 
+const Hotel = db.models.hotel; 
+const Restaurant = db.models.restaurant; 
 
 module.exports = router;
 
 router.get('/', function(req, res, next) {
-	res.render('index');
+
+
+	Promise.all([
+		Hotel.findAll(), 
+		Restaurant.findAll(), 
+		Activity.findAll() 
+		])
+		.then(function(results){
+			res.render('index', {
+				hotels: results[0], 
+				restaurants: results[1], 
+				activities: results[2] 
+			});
+		})
+		.catch(next) 
 });
+
 
 
